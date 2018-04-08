32f8fcf94ac7df74ce1a1c70a8a447323c1bac51
physics-sp <physics-sp@protonmail.com>
2018-04-08 20:09:08 -0300

    modifico prog.c para testear numeros mejor

--- search_and_change/prog.c
+++ search_and_change/prog.c
@@ -5,24 +5,13 @@ int global = 112233;
 
 int main(int argc, char * argv[])
 {
-	char str[] = "texto de prueba";
 	int num = 12345;
-	printf("name = %s\n",  argv[0]);
+	printf("num at %p\n",&num);
+	printf("global at %p\n\n", &global);
 	printf("num = %d\n", num);
-	printf("str = %s\n", str);
-	printf("global = %d\n", global);
-	if(argc > 1)
-	{
-		printf("param = %s\n", argv[1]);
-	}
+	printf("global = %d\n\n", global);
 	sleep(5);
-	printf("\nname = %s\n",  argv[0]);
 	printf("num = %d\n", num);
-	printf("str = %s\n", str);
 	printf("global = %d\n", global);
-	if(argc > 1)
-	{
-		printf("param = %s\n", argv[1]);
-	}
 	return 0;
 }
