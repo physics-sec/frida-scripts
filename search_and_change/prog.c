fa8f527448ceae66de93cfcb89d1d233b5f7cec4
santipecin <santipecin@est.frba.utn.edu.ar>
2018-04-13 14:51:58 -0300

    borro binarios y .c's

--- search_and_change/prog.c
+++ search_and_change/prog.c
@@ -6,10 +6,13 @@ int global = 112233;
 
 int main(int argc, char * argv[])
 {
+	char str[] = "texto de prueba";
 	int64_t num = 12345;
+	printf("str = %s\n", str);
 	printf("num = %ld\n", num);
 	printf("global = %d\n\n", global);
 	sleep(5);
+	printf("str = %s\n", str);
 	printf("num = %ld\n", num);
 	printf("global = %d\n", global);
 	return 0;
