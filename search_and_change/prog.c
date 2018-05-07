5e971d7f9293e65590774e2055b25daecd24fb71
physics-sp <physics-sp@protonmail.com>
2018-05-06 21:37:11 -0300

    prints en loop eterno

--- search_and_change/prog.c
+++ search_and_change/prog.c
@@ -8,12 +8,12 @@ int main(int argc, char * argv[])
 {
 	char str[] = "texto de prueba";
 	int64_t num = 12345;
-	printf("str = %s\n", str);
-	printf("num = %ld\n", num);
-	printf("global = %d\n\n", global);
-	sleep(5);
-	printf("str = %s\n", str);
-	printf("num = %ld\n", num);
-	printf("global = %d\n", global);
+	while(1)
+	{
+		printf("str = %s\n", str);
+		printf("num = %ld\n", num);
+		printf("global = %d\n\n", global);
+		sleep(1);
+	}
 	return 0;
 }
