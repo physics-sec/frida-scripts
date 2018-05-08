9b31fabcfb56878fe3016f3a7d6f33e6f03747d3
physics-sp <physics-sp@protonmail.com>
2018-05-08 17:03:50 -0300

    agrego contador a prog.c y duerme 2 en vez de 1 seg

--- search_and_change/prog.c
+++ search_and_change/prog.c
@@ -8,12 +8,14 @@ int main(int argc, char * argv[])
 {
 	char str[] = "texto de prueba";
 	int64_t num = 12345;
+	int count = 0;
 	while(1)
 	{
+		printf("count = %d\n", count++);
 		printf("str = %s\n", str);
 		printf("num = %ld\n", num);
 		printf("global = %d\n\n", global);
-		sleep(1);
+		sleep(2);
 	}
 	return 0;
 }
