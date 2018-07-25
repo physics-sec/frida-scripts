07ae22e033cec889cd5d6f7e41989f43df5a5420
physics-sp <physics-sp@protonmail.com>
2018-07-25 19:48:59 -0300

    agrego certPinnig de WooyunDota

--- Android/certificatePinning.py
+++ Android/certificatePinning.py
@@ -1,17 +1,6 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# other implementations:
-# https://github.com/Fuzion24/JustTrustMe/blob/master/app/src/main/java/just/trust/me/Main.java
-# https://github.com/ac-pm/Inspeckage/blob/master/app/src/main/java/mobi/acpm/inspeckage/hooks/SSLPinningHook.java
-# https://github.com/datatheorem/TrustKit-Android/tree/master/trustkit/src/main/java/com/datatheorem/android/trustkit/pinning
-# https://gist.github.com/matthewromano/4178946
-# https://github.com/ac-pm/SSLUnpinning_Xposed/tree/master/app/src/main/java/mobi/acpm/sslunpinning
-# https://github.com/iSECPartners/Android-SSL-TrustKiller/tree/master/src/com/android/SSLTrustKiller
-# https://github.com/iSECPartners/android-ssl-bypass
-# https://github.com/WooyunDota/DroidSSLUnpinning
-# https://gist.github.com/oleavr/3ca67a173ff7d207c6b8c3b0ca65a9d8
-
 import sys
 import time
 try:
@@ -19,104 +8,359 @@ try:
 except ImportError:
 	sys.exit('install frida\nsudo pip3 install frida')
 
-def err(msg):
-	sys.stderr.write(msg + '\n')
+def sbyte2ubyte(byte):
+	return (byte % 256)
+
+def print_result(message):
+	print ("[!] Received: [%s]" %(message))
 
 def on_message(message, data):
-	if message['type'] == 'error':
-		err('[!] ' + message['stack'])
-	elif message['type'] == 'send':
-		print('[+] ' + message['payload'])
+	if 'payload' in message:
+		data = message['payload']
+		if type(data) is str:
+			print_result(data)
+		elif type(data) is list:
+			a = data[0]
+			if type(a) is int:
+				hexstr = "".join([("%02X" % (sbyte2ubyte(a))) for a in data])
+				print_result(hexstr)
+				print_result(hexstr.decode('hex'))
+			else:
+				print_result(data)
+				print_result(hexstr.decode('hex'))
+		else:
+			print_result(data)
 	else:
-		print(message)
+		if message['type'] == 'error':
+			print (message['stack'])
+		else:
+			print_result(message)
+
+def kill_process(target_process):
+    cmd = "adb shell pm clear {} 1> /dev/null".format(target_process)
+    os.system(cmd)
 
 def main():
-	pathToCertificate = sys.argv[1]
-	target_process = sys.argv[2]
-	try:
-		started = False
-		session = frida.get_usb_device().attach(target_process)
-	except frida.ProcessNotFoundError:
-		print('Starting process {}...\n'.format(target_process))
-		started = True
-		device = frida.get_usb_device()
-		try:
-			pid = device.spawn([target_process])
-		except frida.NotSupportedError:
-			sys.exit('An error ocurred while attaching with the procces\n')
-		session = device.attach(pid)
-
-	script = session.create_script("""
+	target_process = sys.argv[1]
+	kill_process(target_process)
+	device  = frida.get_usb_device()
+	pid     = device.spawn([target_process])
+	session = device.attach(pid)
+	script  = session.create_script("""
 /*
-	code from:
-	https://techblog.mediaservice.net/2017/07/universal-android-ssl-pinning-bypass-with-frida/
-	https://developer.android.com/training/articles/security-ssl#UnknownCa
+-------------------------------------------------------------------------
+	the js code is from the DroidSSLUnpinning repository of WooyunDota
+	https://github.com/WooyunDota/DroidSSLUnpinning
+-------------------------------------------------------------------------
 */
-Java.perform(function (){
-	console.log("");
-	console.log("[.] Cert Pinning Bypass/Re-Pinning");
+Java.perform(function() {
 
-	var CertificateFactory = Java.use("java.security.cert.CertificateFactory");
-	var FileInputStream = Java.use("java.io.FileInputStream");
-	var BufferedInputStream = Java.use("java.io.BufferedInputStream");
-	var X509Certificate = Java.use("java.security.cert.X509Certificate");
-	var KeyStore = Java.use("java.security.KeyStore");
-	var TrustManagerFactory = Java.use("javax.net.ssl.TrustManagerFactory");
-	var SSLContext = Java.use("javax.net.ssl.SSLContext");
+/*
+hook list:
+1.SSLcontext
+2.okhttp
+3.webview
+4.XUtils
+5.httpclientandroidlib
+6.JSSE
+7.network_security_config (android 7.0+)
+8.Apache Http client (support partly)
+*/
 
-	// Load CAs from an InputStream
-	console.log("[+] Loading our CA...")
-	cf = CertificateFactory.getInstance("X.509");
-	
-	try {
-		var fileInputStream = FileInputStream.$new("%s");
+// Attempts to bypass SSL pinning implementations in a number of
+// ways. These include implementing a new TrustManager that will
+// accept any SSL certificate, overriding OkHTTP v3 check()
+// method etc.
+var X509TrustManager = Java.use('javax.net.ssl.X509TrustManager');
+var HostnameVerifier = Java.use('javax.net.ssl.HostnameVerifier');
+var SSLContext = Java.use('javax.net.ssl.SSLContext');
+var quiet_output = false;
+
+// Helper method to honor the quiet flag.
+function quiet_send(data) {
+
+	if (quiet_output) {
+
+		return;
 	}
-	catch(err) {
-		console.log("[o] " + err);
+
+	send(data)
+}
+
+
+// Implement a new TrustManager
+// ref: https://gist.github.com/oleavr/3ca67a173ff7d207c6b8c3b0ca65a9d8
+// Java.registerClass() is only supported on ART for now(201803). 所以android 4.4以下不兼容,4.4要切换成ART使用.
+/*
+06-07 16:15:38.541 27021-27073/mi.sslpinningdemo W/System.err: java.lang.IllegalArgumentException: Required method checkServerTrusted(X509Certificate[], String, String, String) missing
+06-07 16:15:38.542 27021-27073/mi.sslpinningdemo W/System.err:	 at android.net.http.X509TrustManagerExtensions.<init>(X509TrustManagerExtensions.java:73)
+		at mi.ssl.MiPinningTrustManger.<init>(MiPinningTrustManger.java:61)
+06-07 16:15:38.543 27021-27073/mi.sslpinningdemo W/System.err:	 at mi.sslpinningdemo.OkHttpUtil.getSecPinningClient(OkHttpUtil.java:112)
+		at mi.sslpinningdemo.OkHttpUtil.get(OkHttpUtil.java:62)
+		at mi.sslpinningdemo.MainActivity$1$1.run(MainActivity.java:36)
+*/
+var X509Certificate = Java.use("java.security.cert.X509Certificate");
+var TrustManager;
+try {
+	TrustManager = Java.registerClass({
+		name: 'org.wooyun.TrustManager',
+		implements: [X509TrustManager],
+		methods: {
+			checkClientTrusted: function (chain, authType) {
+			},
+			checkServerTrusted: function (chain, authType) {
+			},
+			getAcceptedIssuers: function () {
+				// var certs = [X509Certificate.$new()];
+				// return certs;
+				return [];
+			}
+		}
+	});
+} catch (e) {
+	console.log("registerClass from X509TrustManager >>>>>>>> " + e.message);
+}
+
+
+
+
+
+// Prepare the TrustManagers array to pass to SSLContext.init()
+var TrustManagers = [TrustManager.$new()];
+
+try {
+	// Prepare a Empty SSLFactory
+	var TLS_SSLContext = SSLContext.getInstance("TLS");
+	TLS_SSLContext.init(null,TrustManagers,null);
+	var EmptySSLFactory = TLS_SSLContext.getSocketFactory();
+} catch (e) {
+	console.log(e.message);
+}
+
+send('Custom, Empty TrustManager ready');
+
+// Get a handle on the init() on the SSLContext class
+var SSLContext_init = SSLContext.init.overload(
+	'[Ljavax.net.ssl.KeyManager;', '[Ljavax.net.ssl.TrustManager;', 'java.security.SecureRandom');
+
+// Override the init method, specifying our new TrustManager
+SSLContext_init.implementation = function (keyManager, trustManager, secureRandom) {
+
+	quiet_send('Overriding SSLContext.init() with the custom TrustManager');
+
+	SSLContext_init.call(this, null, TrustManagers, null);
+};
+
+/*** okhttp3.x unpinning ***/
+
+
+// Wrap the logic in a try/catch as not all applications will have
+// okhttp as part of the app.
+try {
+
+	var CertificatePinner = Java.use('okhttp3.CertificatePinner');
+
+	console.log('OkHTTP 3.x Found');
+
+	CertificatePinner.check.overload('java.lang.String', 'java.util.List').implementation = function () {
+
+		quiet_send('OkHTTP 3.x check() called. Not throwing an exception.');
 	}
-	
-	var bufferedInputStream = BufferedInputStream.$new(fileInputStream);
-	var ca = cf.generateCertificate(bufferedInputStream);
-	bufferedInputStream.close();
 
-	var certInfo = Java.cast(ca, X509Certificate);
-	console.log("[o] Our CA Info: " + certInfo.getSubjectDN());
+} catch (err) {
 
-	// Create a KeyStore containing our trusted CAs
-	console.log("[+] Creating a KeyStore for our CA...");
-	var keyStoreType = KeyStore.getDefaultType();
-	var keyStore = KeyStore.getInstance(keyStoreType);
-	keyStore.load(null, null);
-	keyStore.setCertificateEntry("ca", ca);
-	
-	// Create a TrustManager that trusts the CAs in our KeyStore
-	console.log("[+] Creating a TrustManager that trusts the CA in our KeyStore...");
-	var tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
-	var tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
-	tmf.init(keyStore);
-	console.log("[+] Our TrustManager is ready...");
+	// If we dont have a ClassNotFoundException exception, raise the
+	// problem encountered.
+	if (err.message.indexOf('ClassNotFoundException') === 0) {
 
-	console.log("[+] Hijacking SSLContext methods now...")
-	console.log("[-] Waiting for the app to invoke SSLContext.init()...")
-
-	SSLContext.init.overload("[Ljavax.net.ssl.KeyManager;", "[Ljavax.net.ssl.TrustManager;", "java.security.SecureRandom").implementation = function(a,b,c) {
-		console.log("[o] App invoked javax.net.ssl.SSLContext.init...");
-		SSLContext.init.overload("[Ljavax.net.ssl.KeyManager;", "[Ljavax.net.ssl.TrustManager;", "java.security.SecureRandom").call(this, a, tmf.getTrustManagers(), c);
-		console.log("[+] SSLContext initialized with our custom TrustManager!");
+		throw new Error(err);
 	}
+}
+
+// Appcelerator Titanium PinningTrustManager
+
+// Wrap the logic in a try/catch as not all applications will have
+// appcelerator as part of the app.
+try {
+
+	var PinningTrustManager = Java.use('appcelerator.https.PinningTrustManager');
+
+	send('Appcelerator Titanium Found');
+
+	PinningTrustManager.checkServerTrusted.implementation = function () {
+
+		quiet_send('Appcelerator checkServerTrusted() called. Not throwing an exception.');
+	}
+
+} catch (err) {
+
+	// If we dont have a ClassNotFoundException exception, raise the
+	// problem encountered.
+	if (err.message.indexOf('ClassNotFoundException') === 0) {
+
+		throw new Error(err);
+	}
+}
+
+/*** okhttp unpinning ***/
+
+
+try {
+	var OkHttpClient = Java.use("com.squareup.okhttp.OkHttpClient");
+	OkHttpClient.setCertificatePinner.implementation = function(certificatePinner){
+		// do nothing
+		console.log("OkHttpClient.setCertificatePinner Called!");
+		return this;
+	};
+
+	// Invalidate the certificate pinnet checks (if "setCertificatePinner" was called before the previous invalidation)
+	var CertificatePinner = Java.use("com.squareup.okhttp.CertificatePinner");
+	CertificatePinner.check.overload('java.lang.String', '[Ljava.security.cert.Certificate;').implementation = function(p0, p1){
+		// do nothing
+		console.log("okhttp Called! [Certificate]");
+		return;
+	};
+	CertificatePinner.check.overload('java.lang.String', 'java.util.List').implementation = function(p0, p1){
+		// do nothing
+		console.log("okhttp Called! [List]");
+		return;
+	};
+} catch (e) {
+ console.log("com.squareup.okhttp not found");
+}
+
+/*** WebView Hooks ***/
+
+/* frameworks/base/core/java/android/webkit/WebViewClient.java */
+/* public void onReceivedSslError(Webview, SslErrorHandler, SslError) */
+var WebViewClient = Java.use("android.webkit.WebViewClient");
+
+WebViewClient.onReceivedSslError.implementation = function (webView,sslErrorHandler,sslError){
+	quiet_send("WebViewClient onReceivedSslError invoke");
+	//执行proceed方法
+	sslErrorHandler.proceed();
+	return ;
+};
+
+WebViewClient.onReceivedError.overload('android.webkit.WebView', 'int', 'java.lang.String', 'java.lang.String').implementation = function (a,b,c,d){
+	quiet_send("WebViewClient onReceivedError invoked");
+	return ;
+};
+
+WebViewClient.onReceivedError.overload('android.webkit.WebView', 'android.webkit.WebResourceRequest', 'android.webkit.WebResourceError').implementation = function (){
+	quiet_send("WebViewClient onReceivedError invoked");
+	return ;
+};
+
+/*** JSSE Hooks ***/
+
+/* libcore/luni/src/main/java/javax/net/ssl/TrustManagerFactory.java */
+/* public final TrustManager[] getTrustManager() */
+
+var TrustManagerFactory = Java.use("javax.net.ssl.TrustManagerFactory");
+TrustManagerFactory.getTrustManagers.implementation = function(){
+	quiet_send("TrustManagerFactory getTrustManagers invoked");
+	return TrustManagers;
+}
+
+var HttpsURLConnection = Java.use("javax.net.ssl.HttpsURLConnection");
+/* libcore/luni/src/main/java/javax/net/ssl/HttpsURLConnection.java */
+/* public void setDefaultHostnameVerifier(HostnameVerifier) */
+HttpsURLConnection.setDefaultHostnameVerifier.implementation = function(hostnameVerifier){
+	quiet_send("HttpsURLConnection.setDefaultHostnameVerifier invoked");
+		return null;
+};
+/* libcore/luni/src/main/java/javax/net/ssl/HttpsURLConnection.java */
+/* public void setSSLSocketFactory(SSLSocketFactory) */
+HttpsURLConnection.setSSLSocketFactory.implementation = function(SSLSocketFactory){
+	quiet_send("HttpsURLConnection.setSSLSocketFactory invoked");
+		return null;
+};
+/* libcore/luni/src/main/java/javax/net/ssl/HttpsURLConnection.java */
+/* public void setHostnameVerifier(HostnameVerifier) */
+HttpsURLConnection.setHostnameVerifier.implementation = function(hostnameVerifier){
+	quiet_send("HttpsURLConnection.setHostnameVerifier invoked");
+		return null;
+};
+
+/*** Xutils3.x hooks ***/
+//Implement a new HostnameVerifier
+var TrustHostnameVerifier;
+try {
+	TrustHostnameVerifier = Java.registerClass({
+		name: 'org.wooyun.TrustHostnameVerifier',
+		implements: [HostnameVerifier],
+		method: {
+			verify: function (hostname, session) {
+				return true;
+			}
+		}
+	});
+
+} catch (e) {
+	//java.lang.ClassNotFoundException: Didn't find class "org.wooyun.TrustHostnameVerifier"
+	console.log("registerClass from hostnameVerifier >>>>>>>> " + e.message);
+}
+
+try {
+	var RequestParams = Java.use('org.xutils.http.RequestParams');
+	RequestParams.setSslSocketFactory.implementation = function(sslSocketFactory){
+		sslSocketFactory = EmptySSLFactory;
+		return null;
+	}
+
+	RequestParams.setHostnameVerifier.implementation = function(hostnameVerifier){
+		hostnameVerifier = TrustHostnameVerifier.$new();
+		return null;
+	}
+
+} catch (e) {
+	console.log("Xutils hooks not Found");
+}
+
+/*** httpclientandroidlib Hooks ***/
+try {
+	var AbstractVerifier = Java.use("ch.boye.httpclientandroidlib.conn.ssl.AbstractVerifier");
+	AbstractVerifier.verify.overload('java.lang.String','[Ljava.lang.String','[Ljava.lang.String','boolean').implementation = function(){
+		quiet_send("httpclientandroidlib Hooks");
+		return null;
+	}
+} catch (e) {
+	console.log("httpclientandroidlib Hooks not found");
+}
+
+/***
+android 7.0+ network_security_config TrustManagerImpl hook
+apache httpclient partly
+***/
+var TrustManagerImpl = Java.use("com.android.org.conscrypt.TrustManagerImpl");
+
+try {
+	// Android 7+ TrustManagerImpl
+	TrustManagerImpl.verifyChain.implementation = function (untrustedChain, trustAnchorChain, host, clientAuth, ocspData, tlsSctData) {
+		quiet_send("TrustManagerImpl verifyChain called");
+		// Skip all the logic and just return the chain again :P
+		//https://www.nccgroup.trust/uk/about-us/newsroom-and-events/blogs/2017/november/bypassing-androids-network-security-configuration/
+		// https://github.com/google/conscrypt/blob/c88f9f55a523f128f0e4dace76a34724bfa1e88c/platform/src/main/java/org/conscrypt/TrustManagerImpl.java#L650
+		return untrustedChain;
+	}
+} catch (e) {
+	console.log("TrustManagerImpl verifyChain nout found below 7.0");
+}
+
 });
-""" % pathToCertificate)
+""")
 	script.on('message', on_message)
-	print('[!] Press <Enter> at any time to detach from instrumented program.\n\n')
+	print('[!] Press <Enter> at any time to detach from instrumented program.')
+	print("[*] Intercepting on {} (pid:{})...\n".format(target_process, pid))
 	script.load()
-	if started:
-		device.resume(pid)
+	device.resume(pid)
 	input()
-	session.detach()	
+	session.detach()
 
 if __name__ == '__main__':
 	if len(sys.argv) != 2:
-		usage = 'usage {} <path to certificate in device> <process name or PID>\n'.format(__file__)
+		usage  = 'usage {} <process name>\n'.format(__file__)
 		usage += 'run \'frida-ps -U\' to list processes\n'
 		sys.exit(usage)
 	main()
